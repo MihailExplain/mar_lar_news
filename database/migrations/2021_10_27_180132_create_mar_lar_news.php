ef190ecd36de447108c4d055aff6f0ee790b09ab
MihailExplain <unkvuzutor@gmail.com>
2021-10-29 13:25:13 +0300

    Migration for table articles

---
+++ database/migrations/2021_10_27_180132_create_mar_lar_news.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMarLarNews extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->index('title');
+            $table->string('article');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('articles');
+    }
+}
