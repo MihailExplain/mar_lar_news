57c323c984350bd6e58fef25d82dd1d526883227
MihailExplain <unkvuzutor@gmail.com>
2021-10-29 14:38:40 +0300

    view for index action of news

---
+++ resources/views/news/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2>All news</h2>
+
+    <!-- Bootstrap шаблон... -->
+
+    <div class="panel-body">
+        <!-- Отображение ошибок проверки ввода -->
+    @include('common.errors')
+
+    <!-- Форма новой задачи -->
+
+    </div>
+
+    <!-- TODO: Текущие задачи -->
+@endsection
