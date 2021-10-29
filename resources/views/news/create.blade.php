c388a9bbea2e837dcefd3e335e4e91b00fd4e589
MihailExplain <unkvuzutor@gmail.com>
2021-10-29 15:30:54 +0300

    make create view for news

---
+++ resources/views/news/create.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2>New news</h2>
+    <div class="panel-body">
+    @include('common.errors')
+        <form action="{{ route('news.store') }}" method="POST" class="form-horizontal">
+        {{ csrf_field() }}
+
+        <!-- Имя задачи -->
+            <div class="form-group">
+                <label for="title" class="col-sm-3 control-label">Title</label>
+
+                <div class="col-sm-6">
+                    <input type="text" name="name" id="title" class="form-control">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="article" class="col-sm-3 control-label">Article</label>
+                <div class="col-sm-6">
+                    <input type="text" name="name" id="article" class="form-control">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-6">
+                    <button type="submit" class="btn btn-default">
+                        <i class="fa fa-plus"></i> Добавить задачу
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <a href="{{route('news.create')}}" class="btn btn-success"><i class="fa fa-plus"></i>Create new news</a>
+    <!-- TODO: Текущие задачи -->
+@endsection
